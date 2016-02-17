0cf30f93921d982d6721dcd6a3147e34f7086fe5
Christian McNeill <me@redxian.com>
2016-02-17 16:50:27 +1000

    Added angular apps

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name statRollerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the statRollerApp
+ */
+angular.module('statRollerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
